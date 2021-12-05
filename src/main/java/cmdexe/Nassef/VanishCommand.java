33d47d7ef01ed1fbd8044c73d343106f1c774e33
Nassef <nassefkhachiche1@gmail.com>
2021-12-05 16:40:26 +0100

    changed deprecated hideplayer/showpalyer methods

--- src/main/java/cmdexe/Nassef/VanishCommand.java
+++ src/main/java/cmdexe/Nassef/VanishCommand.java
@@ -5,7 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,22 +16,24 @@ public class VanishCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
         List<Player> vanishedPlayers = new ArrayList<>();
             if (sender instanceof Player) {
                 Player player = (Player) sender;
+                Plugin plugin = (Plugin) sender;
                 if (player.isOp()) {
                     // will unvanish after typing again
                     if (cmd.getName().equalsIgnoreCase("vanish")) {
                         if (vanishedPlayers.contains(player)) {
                             vanishedPlayers.remove(player);
                             for (Player target : Bukkit.getOnlinePlayers()) {
-                                target.showPlayer(player);
+                                target.showPlayer(plugin,player);
                             }
                             player.sendMessage(ChatColor.BOLD + "Vanished!");
                         } else {
                             vanishedPlayers.add(player);
                             for (Player target : Bukkit.getOnlinePlayers()) {
-                                target.hidePlayer(player);
+                                target.hidePlayer(plugin, player);
                             }
                             player.sendMessage(ChatColor.BOLD + "Unvanished!");
                         }
